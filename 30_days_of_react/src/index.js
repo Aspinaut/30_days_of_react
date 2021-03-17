64a0342c57974b9ac646cb49bc0a0ebf9b027492
Vincent Masse <vincentmasse@protonmail.com>
2021-03-17 16:10:41 +0100

    init repo

---
+++ 30_days_of_react/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+const jsxElement = <h1>This is a JSX element</h1>
+const rootElement = document.getElementById('root')
+
+render(jsxElement,rootElement)
